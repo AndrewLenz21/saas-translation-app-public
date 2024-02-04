3c6e8e60617f0a92ed89aa68355e55860e327ea9
AndrewLenz21 <angelitolenz2000@gmail.com>
2024-02-05 00:32:55 +0100

    COMMIT

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,22 +4,27 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 
 // Your web app's Firebase configuration
+//WARNING: USE YOUR CONFIGURATION WITHOUT THE DOTENV
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID,
+  apiKey: process.env.PUBLIC_NEXT_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_NEXT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PUBLIC_NEXT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_NEXT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_NEXT_FIREBASE_SENDER_ID,
+  appId: process.env.PUBLIC_NEXT_FIREBASE_API_ID,
 };
 
+console.log(firebaseConfig);
 // set application
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+console.log(app);
 // set authentication
+console.log(firebaseConfig);
 const auth = getAuth(app);
 // set database
 const db = getFirestore(app);
 // set functions
 const functions = getFunctions(app);
 
-export {db, auth, functions}
+export { db, auth, functions };
